4989e6b9da96ec4401426d10e85056043c845bf4
enricovianello <maverick17586@gmail.com>
2012-09-14 12:31:02 +0200

    server libs refactoring and added -ssl command line option to turn on https connection (if keystore file exists)

--- server/src/main/java/it/grid/storm/JServer.java
+++ server/src/main/java/it/grid/storm/JServer.java
@@ -1,5 +1,11 @@
 package it.grid.storm;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
@@ -9,6 +15,12 @@ import org.eclipse.jetty.server.nio.SelectChannelConnector;
 import org.eclipse.jetty.server.ssl.SslSelectChannelConnector;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,82 +29,62 @@ public class JServer {
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
 
 	private static final int default_port = 8085;
-
+	
+	private String webappsDirectory = "./webapps";
 	private int running_port;
+	
 	private Server server;
 	private QueuedThreadPool threadPool;
-	private Connector connector;
 	private HandlerCollection hc;
 	private ContextHandlerCollection contextHandlerCollection;
+	
 	
 	public JServer() {
 		this(JServer.default_port);
 	}
 	
 	public JServer(int port) {
-		initAsHttpServer(port);
-		log.info("SERVER: No keystore file defined or detected");
-		log.info("SERVER: I'm working on HTTP");
-	}
-
-	public JServer(int port, String keystoreFilepath, String keystorePassword, String trustPassword) {
-		initAsHttpsServer(port, keystoreFilepath, keystorePassword, trustPassword);
-		log.info("SERVER: keystore file detected");
-		log.info("SERVER: I'm working on HTTPS");
-	}
-
-	private void initAsHttpServer(int port) {
 		server = new Server();
 		this.setRunning_port(port);
 		server.setStopAtShutdown(true);
-
+		
 		// Increase thread pool
 		threadPool = new QueuedThreadPool();
 		threadPool.setMaxThreads(100);
 		server.setThreadPool(threadPool);
-
-		// Ensure using the non-blocking connector (NIO)
-		connector = new SelectChannelConnector();
-		connector.setPort(this.getRunning_port());
-		connector.setMaxIdleTime(30000);
-		server.setConnectors(new Connector[] { connector });
-
-		// Init collection of webapps' handlers
-		hc = new HandlerCollection();
-		contextHandlerCollection = new ContextHandlerCollection();
-		hc.setHandlers(new Handler[] { contextHandlerCollection });
-		server.setHandler(hc);
-	}
-	
-	private void initAsHttpsServer(int port, String keystoreFilepath, String keystorePassword, String trustPassword) {
-		server = new Server();
-		this.setRunning_port(port);
-		server.setStopAtShutdown(true);
-
-		// Increase thread pool
-		threadPool = new QueuedThreadPool();
-		threadPool.setMaxThreads(100);
-		server.setThreadPool(threadPool);
-
-		SslSelectChannelConnector ssl_connector = new SslSelectChannelConnector();
-        ssl_connector.setPort(port);
-        ssl_connector.setKeystore(keystoreFilepath);
-        ssl_connector.setKeyPassword(keystorePassword);
-		ssl_connector.setTrustPassword(trustPassword);
-		server.setConnectors(new Connector[] { ssl_connector });
 		
 		// Init collection of webapps' handlers
 		hc = new HandlerCollection();
 		contextHandlerCollection = new ContextHandlerCollection();
 		hc.setHandlers(new Handler[] { contextHandlerCollection });
-		server.setHandler(hc);
+		server.setHandler(hc);	
+	}
+
+	public void initAsHttpServer() {
+		Connector connector = new SelectChannelConnector();
+		connector.setPort(this.getRunning_port());
+		connector.setMaxIdleTime(30000);
+		server.setConnectors(new Connector[] { connector });
+		log.info("SERVER: initialized for working on HTTP");
+	}
+	
+	public void initAsHttpsServer(String keystoreFilepath, String keystorePassword, String trustPassword) {
+		SslSelectChannelConnector ssl_connector = new SslSelectChannelConnector();
+        ssl_connector.setPort(this.getRunning_port());
+        ssl_connector.setKeystore(keystoreFilepath);
+        ssl_connector.setKeyPassword(keystorePassword);
+		ssl_connector.setTrustPassword(trustPassword);
+		ssl_connector.setAllowRenegotiate(false);
+		server.setConnectors(new Connector[] { ssl_connector });
+		log.info("SERVER: initialized for working on HTTPS");
 	}
 	
 	public void start() throws Exception {
+		if (server.getConnectors() == null)
+			throw new Exception("server not initialized!");
 		server.start();
 		log.info("SERVER: I'm working on port " + this.getRunning_port());
 		log.info("SERVER: STARTED");
-		log.info("SERVER: Hello World!!");
 	}
 
 	public void stop() throws Exception {
@@ -104,40 +96,35 @@ public class JServer {
 		server.join();
 	}
 
-	public void deploy(String contextPath, String webappPath) throws Exception {
+	public void deploy(WebApp webAppToDeploy) throws IOException, Exception {
 
-		log.info("SERVER: DEPLOY WEBAPP {" + contextPath + ", " + webappPath
-				+ "} ... STARTED");
+		if (!webAppToDeploy.isReadyToDeploy())
+			throw new Exception("SERVER: the webapp " + webAppToDeploy.toString() + " is not ready to deploy!");
+		
+		String webAppPath = this.getWebappsDirectory() + "/" + webAppToDeploy.getName();
+		
+		log.info("SERVER-DEPLOY: decompressing webapp template file {" + webAppToDeploy.getWarFile() + "} into {"+ this.getWebappsDirectory() +"}");
+		(new Zip()).unzip(webAppToDeploy.getWarFile(), webAppPath);
+		log.info("SERVER-DEPLOY: decompressed! ");
+	
+		String applicationContextFile = webAppPath + "/WEB-INF/classes/applicationContext.xml";	
+		log.info("SERVER-DEPLOY: root directory has to be modified ... ");
+		this.setRootDir(applicationContextFile, webAppToDeploy.getRootDirectory());
+		log.info("SERVER-DEPLOY: root directory fixed! ");
+				
 		WebAppContext context = new WebAppContext();
-		context.setDescriptor(webappPath + "/WEB-INF/web.xml");
-		context.setResourceBase(webappPath);
+		String contextPath = "/" + webAppToDeploy.getName();
+		context.setDescriptor(webAppPath + "/WEB-INF/web.xml");
+		context.setResourceBase(webAppPath);
 		context.setContextPath(contextPath);
-		context.setParentLoaderPriority(true);
+		context.setParentLoaderPriority(true);		
 		contextHandlerCollection.addHandler(context);
 		context.start();
-		log.info("SERVER: DEPLOY WEBAPP {" + contextPath + ", " + webappPath
-				+ "} ... DEPLOYED");
+		
+		log.info("SERVER-DEPLOY: WEBAPP {" + contextPath + ", " + webAppPath + "} ... DEPLOYED");
 
 	}
-
-	public void deployWar(String contextPath, String warPath) throws Exception {
-
-		log.info("SERVER: DEPLOY WEBAPP WAR FILE {" + contextPath + ", " + warPath
-				+ "} ... STARTED");
-
-		String outputDirectory = "/tmp/temp";
-		log.info("SERVER: Decompressing " + warPath + " on directory "
-				+ outputDirectory);
-		(new Zip()).unzip(warPath, outputDirectory);
-		log.info("SERVER: Decompressing OK");
-
-		this.deploy(contextPath, outputDirectory);
-
-		log.info("SERVER: DEPLOY WEBAPP WAR FILE {" + contextPath + ", " + warPath
-				+ "} ... DEPLOYED");
-
-	}
-
+	
 	public int getRunning_port() {
 		return running_port;
 	}
@@ -145,4 +132,77 @@ public class JServer {
 	public void setRunning_port(int running_port) {
 		this.running_port = running_port;
 	}
+	
+	private void setRootDir(String xmlfilesrc, String rootdir) throws Exception {
+		try {			
+			System.out.println("xmlfilesrc: " + xmlfilesrc);
+			SAXBuilder builder = new SAXBuilder();
+			File xmlFile = new File(xmlfilesrc);
+			Document doc = (Document) builder.build(xmlFile);
+
+			Element rootNode = doc.getRootElement();
+			List<?> beans = rootNode.getChildren();
+			Element current = null;
+			Iterator<?> i = beans.iterator();
+			while (i.hasNext()) {
+				current = (Element) i.next();
+				if (current.getAttributeValue("id").equals(
+						"milton.fs.resource.factory"))
+					break;
+			}
+			if (!current.getAttributeValue("id").equals("milton.fs.resource.factory"))
+				throw new Exception("node 'milton.fs.resource.factory' not found!");
+			
+			List<?> properties = current.getChildren();
+			Element property = null;
+			i = properties.iterator();
+			while (i.hasNext()) {
+				property = (Element) i.next();
+				if (property.getAttributeValue("name").equals("root"))
+					break;
+			}
+			if (!property.getAttributeValue("name").equals("root"))
+				throw new Exception("attribute 'root' not found!");
+			
+			property.setAttribute("value", rootdir);
+
+			XMLOutputter xmlOutput = new XMLOutputter();
+
+			// display nice nice
+			xmlOutput.setFormat(Format.getPrettyFormat());
+			xmlOutput.output(doc, new FileWriter(xmlfilesrc));
+
+		} catch (IOException io) {
+			io.printStackTrace();
+		} catch (JDOMException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getWebappsDirectory() {
+		return webappsDirectory;
+	}
+
+	public void setWebappsDirectory(String webappsDirectory) {
+		this.webappsDirectory = webappsDirectory;
+	}
+	
+	public boolean deleteWebApps() {
+		return deleteDirectory(new File(this.getWebappsDirectory()));
+	}
+	
+	private boolean deleteDirectory(File path) {
+		if (path.exists()) {
+			File[] files = path.listFiles();
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].isDirectory()) {
+					deleteDirectory(files[i]);
+				} else {
+					files[i].delete();
+				}
+			}
+		}
+		return (path.delete());
+	}
+	
 }
