575dcccddd72f6631251ee5c35d41615dd48e079
enricovianello <maverick17586@gmail.com>
2012-11-29 12:44:02 +0100

    Fixed srmLs fro Get Directory file list??

--- webapp/src/main/java/it/grid/storm/webdav/webapp/factory/StormDirectoryResource.java
+++ webapp/src/main/java/it/grid/storm/webdav/webapp/factory/StormDirectoryResource.java
@@ -8,6 +8,8 @@ import io.milton.http.exceptions.BadRequestException;
 import io.milton.http.exceptions.ConflictException;
 import io.milton.http.exceptions.NotAuthorizedException;
 import io.milton.resource.*;
+import it.grid.storm.srm.types.Recursion;
+import it.grid.storm.srm.types.TFileType;
 import it.grid.storm.xmlrpc.outputdata.LsOutputData.SurlInfo;
 
 import java.io.File;
@@ -124,7 +126,7 @@ public class StormDirectoryResource extends StormResource implements MakeCollect
 		w.open("body");
 		w.begin("h1").open().writeText(this.getName()).close();
 		w.open("table");
-		for (SurlInfo entry : StormResourceHelper.doLs(this)) {
+		for (SurlInfo entry : StormResourceHelper.doLsDetailed(this, Recursion.FULL).get(0).getSubpathInfo()) {
 			w.open("tr");
 			w.open("td");
 			String name = entry.getStfn().split("/")[entry.getStfn().split("/").length-1];
