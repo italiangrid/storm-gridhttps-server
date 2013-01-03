65aaed43a27bd33208c935b55fd9c215dc391da0
enricovianello <maverick17586@gmail.com>
2013-01-03 16:40:57 +0100

    copy inputstream to outputstream while calculating adler32 checksum on contentservice class

--- webapp/src/main/java/it/grid/storm/webdav/factory/StormContentService.java
+++ webapp/src/main/java/it/grid/storm/webdav/factory/StormContentService.java
@@ -1,6 +1,9 @@
 package it.grid.storm.webdav.factory;
 
 import io.milton.http.fs.FileContentService;
+import it.grid.storm.checksum.ChecksumFileReadException;
+import it.grid.storm.checksum.ChecksumNotSupportedException;
+import it.grid.storm.checksum.Checksum.ChecksumAlgorithm;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -8,41 +11,125 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.zip.Adler32;
+import java.util.zip.CRC32;
+import java.util.zip.CheckedInputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class StormContentService implements FileContentService {
 
-	@Override
+	private static final Logger log = LoggerFactory.getLogger(StormResourceFactory.class);
+	private static final int BUFFER_SIZE = 4096;
+
 	public void setFileContent(File file, InputStream in) throws FileNotFoundException, IOException {
-		FileOutputStream out = null;
-		byte[] toMatch = new byte[] {97, 98, 99}; //abc
-		try {
-			out = new FileOutputStream(file);
-			byte[] buffer = new byte[3]; // To hold file contents
-			int bytes_read; // How many bytes in buffer
-			while ((bytes_read = in.read(buffer)) != -1) {
-				out.write(buffer, 0, bytes_read); // write
-				if ((new String(buffer)).equals(new String(toMatch)))
-					System.out.println("+");
-			}
-		} finally {
-			if (in != null)
-				try {
-					out.close();
-				} catch (IOException e) {
-					;
-				}
-			if (out != null)
-				try {
-					out.close();
-				} catch (IOException e) {
-					;
-				}
-		}
+		String checksum = computeCopyWithChecksum(in, new FileOutputStream(file), ChecksumAlgorithm.ADLER32.name());
+		System.out.println("checksum: " + checksum);
 	}
 
-	@Override
 	public InputStream getFileContent(File file) throws FileNotFoundException {
 		FileInputStream fin = new FileInputStream(file);
 		return fin;
 	}
+
+	private String computeCopyWithChecksum(InputStream inputStream, FileOutputStream outputStream, String algorithm) {
+
+		ChecksumAlgorithm checksumAlgorithm = ChecksumAlgorithm.getChecksumAlgorithm(algorithm);
+		if (checksumAlgorithm == null) {
+			throw new ChecksumNotSupportedException("Checksum algorithm not supported: " + algorithm);
+		}
+		String checksum;
+		try {
+			if (checksumAlgorithm == ChecksumAlgorithm.CRC32) {
+				log.debug("Computing " + ChecksumAlgorithm.CRC32);
+				checksum = computeCopyWithChecksumCRC32(inputStream, outputStream);
+			} else if (checksumAlgorithm == ChecksumAlgorithm.ADLER32) {
+				log.debug("Computing " + ChecksumAlgorithm.ADLER32);
+				checksum = computeCopyWithChecksumAdler32(inputStream, outputStream);
+			} else {
+				checksum = computeCopyWithChecksumMD(inputStream, outputStream, checksumAlgorithm);
+			}
+		} catch (IOException e) {
+			throw new ChecksumFileReadException("Error reading inputstream", e);
+		} catch (NoSuchAlgorithmException e) {
+			throw new ChecksumNotSupportedException("Checksum algorithm not supported: " + algorithm);
+		} finally {
+			if (inputStream != null)
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					log.warn("Some errors occured closing inputstream");
+				}
+			if (outputStream != null)
+				try {
+					outputStream.close();
+				} catch (IOException e) {
+					log.warn("Some errors occured closing outputstream");
+				}	
+		}
+		return checksum;
+	}
+
+	private static String computeCopyWithChecksumAdler32(InputStream inputStream, FileOutputStream outputStream)
+            throws IOException {
+
+        byte[] bArray = new byte[BUFFER_SIZE];
+        CheckedInputStream cis = new CheckedInputStream(inputStream, new Adler32());
+        int bytes_read;
+        while ((bytes_read = cis.read(bArray)) != -1) {
+        	outputStream.write(bArray, 0, bytes_read);
+        }
+        String checksum = Long.toHexString(cis.getChecksum().getValue());
+        bArray = null;
+        return checksum;
+    }
+
+    private static String computeCopyWithChecksumCRC32(InputStream inputStream, FileOutputStream outputStream)
+            throws IOException {
+        
+    	String checksum = null;
+        byte[] bArray = new byte[BUFFER_SIZE];
+        CRC32 crc32 = new CRC32();
+        while (true) {
+            int count = inputStream.read(bArray, 0, BUFFER_SIZE);
+            if (count == -1) {
+                break;
+            }
+            crc32.update(bArray, 0, count);
+            outputStream.write(bArray, 0, count);
+        }
+        bArray = null;
+        checksum = Long.toHexString(crc32.getValue());
+        return checksum;
+    }
+
+    private static String computeCopyWithChecksumMD(InputStream inputStream, FileOutputStream outputStream,
+            ChecksumAlgorithm checksumType) throws IOException, NoSuchAlgorithmException {
+
+        String algorithm = checksumType.toString();
+        byte[] bArray = new byte[BUFFER_SIZE];
+        MessageDigest md = MessageDigest.getInstance(algorithm);
+        log.debug("Computing " + algorithm);
+        while (true) {
+            int count = inputStream.read(bArray, 0, BUFFER_SIZE);
+            if (count == -1) {
+                break;
+            }
+            md.update(bArray, 0, count);
+            outputStream.write(bArray, 0, count);
+        }
+        byte[] hash = md.digest();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < hash.length; i++) {
+            sb.append(Integer.toString((hash[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        bArray = null;
+        hash = null;
+        return sb.toString();
+    }
+	
+	
 }
