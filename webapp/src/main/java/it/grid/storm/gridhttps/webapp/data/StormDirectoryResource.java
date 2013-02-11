ae7797491da8259781ae80579987d4f2fee398f3
enricovianello <maverick17586@gmail.com>
2013-02-11 17:00:19 +0100

    adjustments

--- webapp/src/main/java/it/grid/storm/gridhttps/webapp/data/StormDirectoryResource.java
+++ webapp/src/main/java/it/grid/storm/gridhttps/webapp/data/StormDirectoryResource.java
@@ -127,7 +127,7 @@ public class StormDirectoryResource extends StormResource implements MakeCollect
 		ArrayList<StormResource> list = new ArrayList<StormResource>();
 		Collection<SurlInfo> entries = null;
 		try {
-			entries = StormResourceHelper.doLsDetailed(this, Recursion.LIMITED, numberOfChildren);
+			entries = StormResourceHelper.doLsDetailed(this, Recursion.NONE, numberOfChildren);
 		} catch (TooManyResultsException e) {
 			log.error("The number of children requested for '" + this.getStfn() + "' is greater than the max number of results allowed: " + e.getStatus().getExplanation()); 
 			return list;
