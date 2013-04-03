edd3b6a323933a57eb859337ab80f8c7e3414da8
enricovianello <maverick17586@gmail.com>
2013-04-03 14:48:17 +0200

    print stack trace on stderr in case of runtime/api exceptions

--- src/main/java/it/grid/storm/gridhttps/webapp/StormStandardFilter.java
+++ src/main/java/it/grid/storm/gridhttps/webapp/StormStandardFilter.java
@@ -64,10 +64,12 @@ public class StormStandardFilter implements Filter {
 			}
 		} catch (IllegalArgumentException ex) {
 			log.error("IllegalArgumentException: " + ex.getMessage());
+			System.err.println(ex.getStackTrace());
 			response.sendError(Status.SC_INTERNAL_SERVER_ERROR, ex.getMessage());
 			response.setStatus(Status.SC_INTERNAL_SERVER_ERROR);
 		} catch (RuntimeException ex) {
 			log.error("RuntimeException: " + ex.getMessage());
+			System.err.println(ex.getStackTrace());
 			response.sendError(Status.SC_INTERNAL_SERVER_ERROR, ex.getMessage());
 			response.setStatus(Status.SC_INTERNAL_SERVER_ERROR);
 		} catch (TooManyResultsException ex) {
@@ -76,6 +78,7 @@ public class StormStandardFilter implements Filter {
 			response.setStatus(Status.SC_SERVICE_UNAVAILABLE);
 		} catch (RuntimeApiException ex) {
 			log.error("RuntimeApiException: " + ex.getReason());
+			System.err.println(ex.getStackTrace());
 			response.sendError(Status.SC_INTERNAL_SERVER_ERROR, ex.getReason());
 			response.setStatus(Status.SC_INTERNAL_SERVER_ERROR);
 		} catch (StormRequestFailureException ex) {
