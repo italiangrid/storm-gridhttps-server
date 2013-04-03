87c5b10f6b9eb0216572fbde31f09145b3c898e8
enricovianello <maverick17586@gmail.com>
2013-04-03 15:18:44 +0200

    waitfor cleaned

--- src/main/java/it/grid/storm/gridhttps/server/Main.java
+++ src/main/java/it/grid/storm/gridhttps/server/Main.java
@@ -41,7 +41,7 @@ public class Main {
 		System.out.println("StoRM Gridhttps-server");
 		System.out.println("bootstrapping...");
 
-		waitfor(5000);
+//		waitfor(5000);
 
 		try {
 			parseCommandLine(args);
